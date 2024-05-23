77e4e657d2fb1445d3943fd3214cbbd40758d4d1
Sebikaeb <sebika.ebpearls@gmail.com>
2024-05-23 15:29:57 +0545

    spinner added

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
   if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
